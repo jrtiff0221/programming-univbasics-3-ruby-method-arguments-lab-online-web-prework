851ca2dbd7c59d34fd96689fe47be0957907b7a2
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-08-28 19:55:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,10 @@
-def #introduction
+def introduction
   puts "Hi,my name is #{name}"
-end
-def #introduction_with_language(name, language)
+  end
+def introduction_with_language(name, language)
   puts "Hi,my name is #{name} and I am learning to program in #{language}."
 end
-def #introduction_with_language_optional(name, language="Ruby")
+def introduction_with_language_optional(name, language="Ruby")
   puts "Hi,my name is #{name} and I am learning to program in #{language}."
 end
 
